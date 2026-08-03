19c7749914a453c348191f48d73a6ccd8d4b178a
Carlos Humberto <camehudev@gmail.com>
2026-08-03 16:03:20 -0300

    Criado WebConfig

---
+++ src/main/java/com/example/gastos_fianceiros/infrastructure/security/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.gastos_fianceiros.infrastructure.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica para todas as rotas da API
+                .allowedOrigins(
+                    "https://ui-resqistro-gasto.vercel.app", // URL da sua aplicação na Vercel
+                    "http://localhost:5000"                  // Caso queira testar build local simulando produção
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // Métodos permitidos
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
